b00f6f62533d9eb8f4094fd066e9215707ea12c0
DuroCodes <davidwright13503@gmail.com>
2024-08-18 21:38:22 -0400

    fix: add site to astro config to fix og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bin.durocodes.me',
   integrations: [tailwind(), preact()],
   output: 'server',
   adapter: vercel()
