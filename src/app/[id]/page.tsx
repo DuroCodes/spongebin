843ea6d2e3d7dcb35671dc9c0107701d9a62656a
DuroCodes <davidwright13503@gmail.com>
2025-05-27 14:31:32 -0400

    feat: add test codeblock preview

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -39,10 +39,17 @@ export async function generateMetadata({ params }: Props) {
     };
 
   const numLines = paste.content.split("\n").length;
+  const codeblockPreview = paste.content
+    .split("\n")
+    .slice(0, 3)
+    .join("\n")
+    .replace(/`/g, "\\`")
+    .replace(/^\s*```(\w+)?/, "```" + paste.language)
+    .replace(/^\s*```/, "```");
 
   return {
     title: `spongebin • ${paste.id}`,
-    description: `a paste containing ${numLines} lines of ${paste.language}`,
+    description: `a paste containing ${numLines} lines of ${paste.language}\ncode:\n${codeblockPreview}`,
     openGraph: { images: "/sponge.png" },
     twitter: { card: "summary" },
   };
